fcfc7dc2ab3f97b743733798a594a72142164e72
Johan Swensson <johan@redhat.com>
2026-01-19 14:07:03 +0100

    drop wget dep and save to /tmp

--- getcodiumrpm.sh
+++ getcodiumrpm.sh
@@ -1,2 +1,3 @@
 #!/bin/bash -x
-wget $(curl -s https://api.github.com/repos/VSCodium/vscodium/releases | jq '.[0].assets' | grep 'x86_64.rpm' | grep browser_download_url | head -1 | awk '{ print $2 }' | sed s/\"//g)
+pushd /tmp
+curl -L -O $(curl -s https://api.github.com/repos/VSCodium/vscodium/releases | jq '.[0].assets' | grep 'x86_64.rpm' | grep browser_download_url | head -1 | awk '{ print $2 }' | sed s/\"//g)
